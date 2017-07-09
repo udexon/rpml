3385cca72d94e5861d0d26847bccf7a2107d5238
augustus <lsn95r@gmail.com>
2017-07-09 23:23:51 +0800

    20170709 added new commands

--- log.php
+++ log.php
@@ -1,15 +1,25 @@
 <?php
 
-$id = file_get_contents('php://input'); 
+
+ $id = file_get_contents('php://input'); 
+
+// $id = file_get_contents("j1"); 
+// var_dump($id);
 
 $logFile = "view.log";
+
+// $id = $_POST['id'].'\n';
+//$id = $_POST['id']."\n";
 
 file_put_contents($logFile, $id."\n", FILE_APPEND);
 
 $a=json_decode($id,true);
 // RPML first command, need to access members of $a
+// var_dump($a);
 
 $b=json_decode($a[1][0],true);
+
+// var_dump($b);
 
 $n=$b[0][0];
 
@@ -17,14 +27,14 @@ $la['cmd']=array();
 $lb=&$la['cmd'];
 
 foreach($a as $k => $c) {
-    $lb[]=array($k, gettype($c));
-    // $lb[]=gettype($c);
+$lb[]=array($k, gettype($c));
+// $lb[]=gettype($c);
 }
 
 // input output need to be string
 // final output need to concatenate first output to second output
 // or show only final output
-// see dbg: below
+// or let this be option?
 
 // $a has current input from ajax
 // $la is results of first command
@@ -32,38 +42,100 @@ foreach($a as $k => $c) {
 
 $lc = array();
 
-// main loop for all RPML commands
 foreach($a[1] as $k => $c) {
 
-    $lb[]=array($k,$c,gettype($c));
-    // $lb[]=gettype($c);
+$lb[]=array($k,$c,gettype($c));
+// $lb[]=gettype($c);
 
-    // put this is the loop, after matching first command
-    $j = json_decode($c, true);
-    $n = $j[0][0];
-    $r = $j[0];
+// put this is the loop, after matching first command
+$j = json_decode($c, true);
+// $n = $j[0][0];
+$r = $j[0];
 
-    $l=count($j[0])-1;
-    $n=$j[0][$l];
+// 20170709 $r is [ arg cmd: ]
+$l=count($j[0])-1;
+$n=$j[0][$l];
+$n1=$j[0][$l-1];
 
-    if ((int)($n)>0) { 
-        $la['log'] =  getline($n, $logFile);
-        $lc[$k] = 'log';
-    }
-    // This is output for first command
-    // How to pass to next command?
-    // use index of array as indicator of output
-    else if ($n=='g') {
-        $la['grep'] = preg_grep( "/".$r[1]."/", $la['log']);
-        $lc[$k] = 'grep';
-    }
-    else if ($n=='dbg') $lc[$k] = 'dbg';
-    // $lb[]=array($j,count($j));
-    // $lb[]=array($j[0],count($j[0]));
+if ((int)($n)>0) { $la['log'] =  getline($n, $logFile);
+
+$lc[$k] = 'log';
+}
+// This is output for first command
+// How to pass to next command
+// use index of array as indicator of output?
+else if ($n=='g') {
+$ki=$lc[$k-1];
+
+// $la['grep'] = preg_grep( "/".$r[1]."/", $la['log']);
+
+$la['grep'] = preg_grep( "/".$r[1]."/", $la[$ki]);
+
+$lc[$k] = 'grep';
+}
+else if ($n=='dbg') $lc[$k] = 'dbg';
+
+else if ($n=='l') {
+$lc[$k] = 'l';
+$la['l'] = getlength($logFile);
+}
+else if ($n=='b') {
+$lc[$k] = 'b';
+$la['b'] = getline($la['l'], $logFile);
+}else if ($n=='t') {
+$lc[$k] = 't';
+$la['t'] = getline_top($la['l'], $logFile);
+}
+
+else if ($n=='c') {
+$ki=$lc[$k-1];
+$lc[$k] = 'c';
+$la['c'] = count($la[$ki]);
+}
+else if ($n=='i') {
+$ki=$lc[$k-1];
+$lc[$k] = 'i';
+$la['i'] = $la[$ki][$n1];
+// $n1;
+// $r; // view $r to debug
+
+ // count($la[$ki]);
+}
+else if ($n=='k') {
+$ki=$lc[$k-1];
+$lc[$k] = 'k';
+$la['k'] = array_keys(($la[$ki])); //[$n1]; cannot trim if not string
+// $r; // view $r to debug
+
+ // count($la[$ki]);
+}
+else if ($n=='gt') {
+$ki=$lc[$k-1];
+$lc[$k] = 'gt';
+$la['gt'] = gettype($la[$ki]); 
+//[$n1];
+// $r; // view $r to debug
+
+ // count($la[$ki]);
+}
+else if ($n=='d') {
+$ki=$lc[$k-1];
+$lc[$k] = 'd';
+$la['d'] = json_decode($la[$ki],true); 
+//[$n1];
+// $r; // view $r to debug
+
+ // count($la[$ki]);
+}
+
+// $lb[]=array($j,count($j));
+// $lb[]=array($j[0],count($j[0]));
+
 }
 
 // $la is used to store input output between commands
 // Sections of $la may be deleted for shorter final output
+// How to control options for final output?
 
 $la['seq'] = $lc;
 $cc = count($lc)-1;
@@ -74,20 +146,43 @@ $kl = $lc[$cc];
 if ($kl=='dbg') $lf=$la;
 else $lf = $la[$kl];
 
-// Send this back to web page:
-echo date("Y-m-d H:i:s")."> ".$a[0]." _ ".$a[1][0]." _ ". (is_int($n)?"int":"ltr"). " <p>" . sprintf("%d", (int)($n)). json_encode($lf);
+echo date("Y-m-d H:i:s")."> ".$a[0]." _ ".$a[1][0]." _ ". (is_int($n)?"int":"ltr"). " <p>" . sprintf("%d %s
+
+", (int)($n)). json_encode($lf);
 // count($b)." ".
 
 function getline($n, $f)
 {
-    $a=file($f);
-    $l=count($a);
+$a=file($f);
+$l=count($a);
 
-    $b=array();
+$b=array();
 
-    for($i=0; $i<$n; $i++)
-    $b[]=$a[$l-$i-1];
+for($i=0; $i<$n; $i++)
+$b[]=$a[$l-$i-1];
 
-    return $b;
+return $b;
 }
+
+function getline_top($n, $f)
+{
+$a=file($f);
+$l=count($a);
+
+$b=array();
+
+for($i=0; $i<$n; $i++)
+$b[]=$a[$i];
+
+return $b;
+}
+
+function getlength( $f )
+{
+$a=file($f);
+$l=count($a);
+
+return $l;
+}
+
 ?>
